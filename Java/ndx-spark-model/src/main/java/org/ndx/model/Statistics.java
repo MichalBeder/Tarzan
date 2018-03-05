7bbffe40af2cc19cdb7d8accabed77604f352b2c
Michal Beder <michal.beder@gmail.com>
2018-03-05 03:56:24 +0100

    Refactoring and support for JsonPacket.

--- Java/ndx-spark-model/src/main/java/org/ndx/model/Statistics.java
+++ Java/ndx-spark-model/src/main/java/org/ndx/model/Statistics.java
@@ -3,29 +3,31 @@ package org.ndx.model;
 import org.ndx.model.ConversationModel.FlowAttributes;
 import org.ndx.model.FlowModel.FlowKey;
 import java.util.Date;
+
 public class Statistics {
-    static long UnixBaseTicks = 621355968000000000L; 
-    static long TickPerMicroseconds = 10L; 
-    static long TickPerMilliseconds = 10000L;
-    static long TickPerSeconds = 10000000L;
-    public static Date ticksToDate(long ticks)
-    {
-            long us = (ticks-UnixBaseTicks) / TickPerMicroseconds ;
+    private static long UNIX_BASE_TICKS = 621355968000000000L;
+    private static long TICK_PER_MICROSECONDS = 10L;
+    static long TICK_PER_MILLISECONDS = 10000L;
+    private static long TICK_PER_SECONDS = 10000000L;
+
+    private static java.text.DecimalFormat df = new java.text.DecimalFormat("#.###");
+
+    public static Date ticksToDate(long ticks) {
+            long us = (ticks - UNIX_BASE_TICKS) / TICK_PER_MICROSECONDS;
             return new Date(us/1000);
     }
 
     public static float ticksToSeconds(long ticks)
     {
-        return (((float)ticks) / (float)TickPerSeconds);
+        return (((float)ticks) / (float) TICK_PER_SECONDS);
     }
 
  
-    public static FlowAttributes merge (FlowAttributes x, FlowAttributes y) 
-    {
+    public static FlowAttributes merge (FlowAttributes x, FlowAttributes y) {
         FlowAttributes.Builder builder  = FlowAttributes.newBuilder();
         builder.setFirstSeen(Math.min(x.getFirstSeen(),y.getFirstSeen()));
         builder.setLastSeen(Math.max(x.getLastSeen(),y.getLastSeen()));
-        builder.setPackets(x.getPackets()+y.getPackets());
+        builder.setPackets(x.getPackets() + y.getPackets());
         builder.setOctets(x.getOctets() + y.getOctets());
         builder.setMaximumPayloadSize(Math.max(x.getMaximumPayloadSize(),y.getMaximumPayloadSize()));
         builder.setMinimumPayloadSize(Math.min(x.getMinimumPayloadSize(),y.getMinimumPayloadSize()));
@@ -33,8 +35,7 @@ public class Statistics {
         return builder.build();
     }
 
-    public static FlowAttributes fromPacket(Packet p)
-    {
+    public static FlowAttributes fromPacket(Packet p) {
         Long first = ((Number)p.get(Packet.TIMESTAMP)).longValue();
         Long last = ((Number)p.get(Packet.TIMESTAMP)).longValue();
         Long octets = ((Number)p.get(Packet.LEN)).longValue();
@@ -50,10 +51,24 @@ public class Statistics {
         return builder.build();
     }
 
-    static java.text.DecimalFormat df = new java.text.DecimalFormat("#.###");
+    public static FlowAttributes fromPacket(JsonPacket p) {
+        Long first = ((Number)p.get(Packet.TIMESTAMP)).longValue();
+        Long last = ((Number)p.get(Packet.TIMESTAMP)).longValue();
+        Long octets = ((Number)p.get(Packet.LEN)).longValue();
+
+        FlowAttributes.Builder builder  = FlowAttributes.newBuilder();
+        builder.setFirstSeen(first);
+        builder.setLastSeen(last);
+        builder.setPackets(1);
+        builder.setOctets(octets);
+        builder.setMaximumPayloadSize(octets.intValue());
+        builder.setMinimumPayloadSize(octets.intValue());
+        builder.setMeanPayloadSize(octets.intValue());
+        return builder.build();
+    }
+
     // Formats 
-    public static String format(String flowkey, FlowAttributes attributes)
-    {
+    public static String format(String flowkey, FlowAttributes attributes) {
         Date first = ticksToDate(attributes.getFirstSeen());
      
         Date last = ticksToDate(attributes.getLastSeen());
@@ -63,6 +78,7 @@ public class Statistics {
             fkey.getProtocol().toStringUtf8(), 
             fkey.getSourceAddress().toStringUtf8() + ":" + fkey.getSourceSelector().toStringUtf8(), 
             fkey.getDestinationAddress().toStringUtf8() + ":" + fkey.getDestinationSelector().toStringUtf8());
-        return String.format("%30s %12s %60s %10d %15d %5d",first.toString(), df.format(diff), fkeystr, attributes.getPackets(), attributes.getOctets(), 1 );
+        return String.format("%30s %12s %60s %10d %15d %5d",first.toString(), df.format(diff), fkeystr,
+                attributes.getPackets(), attributes.getOctets(), 1 );
     }
 }
